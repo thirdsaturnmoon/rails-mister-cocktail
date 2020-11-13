fcac707cf4709b15ef72db7291023c64c538e59a
Anna Ptasinska <itsme@hellothisisanna.com>
2020-11-13 15:16:48 +0100

    Initial commit

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,16 +1,32 @@
 class CocktailsController < ApplicationController
+  before_action :find_cocktail, only: [:show, :edit, :update, :destroy]
+
   def index
     @cocktails = Cocktail.all
   end
 
   def show
-    @cocktail = Cocktail.find(params[:id])
   end
 
   def new
     @cocktail = Cocktail.new
   end
 
-  # def create
-  # end
+  def edit
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,15 @@ Rails.application.routes.draw do
   root to: 'page#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cocktails do
-    resources :doses, only [:create, :index, :new]
+    resources :doses, only: [:create, :index, :new]
   end
-  resources :doses, only [:destroy]
+  resources :doses, only: [:destroy]
 end
+
+# get 'cocktails', to: 'cocktails#index', as: :cocktails
+# get 'cocktails/:id' to: 'cocktails#show', as: :cocktail
+# get 'cocktails/new' to: 'cocktails#new', as: :new_cocktail
+# post 'cocktails', to 'cocktails#create'
+# get 'cocktails/:id/edit', to: 'cocktails#edit', as: :edit_cocktail
+# patch 'cocktails/:id', to: 'cocktails#edit'
+# delete 'cocktails/:id', to: 'cocktails#destroy'
